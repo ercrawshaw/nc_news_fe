90964d89b17617132eae87517ae40b99cd6b866e
Your Nameellecrawshaw <eleanorcrawshaw96@gmail.com>
2024-02-06 08:59:52 +0000

    small changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,24 +1,24 @@
-import Container from 'react-bootstrap/Container';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import UserContext from '../Components/UserContext';
-import { log } from 'console';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
 const Login = () => {
 
   document.title = "NC News | Login"
 
-  const loggedInUser = useContext(UserContext);
-
-  
-
-  const navigate = useNavigate()
-
-  return (
-    <section>
-        <h1>Log In</h1>
-    </section>
-  )
+    return (
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src="https://png.pngtree.com/png-vector/20190508/ourmid/pngtree-gallery-vector-icon-png-image_1027754.jpg" roundedCircle />
+          </Col>
+        </Row>
+      </Container>
+    );
 }
 
 export default Login
